773c2b7a325e742dd40b48801445c1c296f0abb7
heiying <172341428@qq.com>
2018-09-22 23:36:32 +0800

    add annotations

---
+++ src/main/java/com/study/concurrency/annotations/ThreadSafe.java
@@ -0,0 +1,15 @@
+package com.study.concurrency.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记线程安全的类或代码
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+    String value() default "";
+}
---
+++ src/main/java/com/study/concurrency/annotations/UnRecommend.java
@@ -0,0 +1,15 @@
+package com.study.concurrency.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记不推荐的写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface UnRecommend {
+    String value() default "";
+}
